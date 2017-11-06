11819ad071342b9f15a658063e413fe7682ac69d
ItsJustChewbacca <itsjustchewbacca@gmail.com>
2017-11-06 22:42:25 +0000

    adding more looping files

---
+++  while-looping-ingredients.js
@@ -0,0 +1,3 @@
+var ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
+var i = 0
+while (i < ingredients.length) { console.log(ingredients[i]); i++ }
---
+++ ingredients.js
@@ -0,0 +1,2 @@
+var ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
+for( var i = 7; i >= 0; i--) { console.log(ingredients[i]) }
