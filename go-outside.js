43b752d46ec73e91f6ab636badd222f8516c8118
ItsJustChewbacca <itsjustchewbacca@gmail.com>
2017-11-06 05:56:11 +0000

    adding all recent files from module 4, also if-statements is a empty file

---
+++ go-outside.js
@@ -0,0 +1,9 @@
+var temperature = 12;
+if (temperature < 0) {console.log("make sure you pick out a scarf!")}
+else if (temperature < 15) {console.log("short sleeves won't cut it!")}
+else {console.log("short sleeves are fine.")}
+console.log("Now you're ready to go outside!")
+if (isCitizen && age > 18) {console.log("you are eligible to vote.")}
+if (temperature < -40 || temperature > 40) {console.log("Maybe going outside isn't such a great idea...")}
+if (!raining) {console.log("leave your umbrella at home!")}
+
---
+++ howmanyhundreds.js
@@ -0,0 +1,7 @@
+function howManyHundreds(num) { return Math.floor(num / 100) }
+howManyHundreds(1234)
+howManyHundreds(55)
+howManyHundreds(100)
+howManyHundreds(1234)
+howManyHundreds(200)
+howManyHundreds(123456)
